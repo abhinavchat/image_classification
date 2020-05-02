7379a59957661644208cca34a077b2ef6608b574
Abhinav Chaturvedi <achaturvedi@calance.com>
2020-05-02 19:14:26 +0530

    Add created_at column to user table.

--- image_classification/models.py
+++ image_classification/models.py
@@ -1,12 +1,14 @@
 from image_classification import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     password_hash = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow())
 
     @property
     def password(self):
